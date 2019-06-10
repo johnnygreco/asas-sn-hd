c9f133f22f71887c03111bcedf2df196d1bd48d0
telastrus <10052313+telastrus@users.noreply.github.com>
2019-06-10 16:38:23 -0400

    made a few semitested changes; new algorithm based on smoothed data

--- playgrounds/scanner.py
+++ playgrounds/scanner.py
@@ -45,7 +45,7 @@ def cut_corners(objlist, thresh=30, size=[2048, 2048]):
             if not (x < thresh and y > (size[1] - thresh)) and not (x > (size[0] - thresh) and y > (size[1] - thresh)):
                 yield i
 
-def find_lbg(objects, maxtries=10, percentiles=(0,90), corners=False):
+def find_lbg_old(objects, maxtries=10, percentiles=(0,90), corners=False):
     if not corners: objects = np.array(list(cut_corners(objects, thresh=500)))
 
     avgsb = np.mean(objects['cflux'] / objects['npix'])
@@ -58,6 +58,35 @@ def find_lbg(objects, maxtries=10, percentiles=(0,90), corners=False):
         if sb < avgsb:
             return obj
     return None
+
+def find_lbg(objects, data, maxtries=10, percentiles=(0,90), corners=False):
+    largest = sorted(objects, key = lambda x: x['npix'], reverse=True)
+    for obj in largest:
+        if is_lbg(obj, data): return obj
+
+def is_lbg(obj, data, default=[30, 2030], extend=30, sigma=1000):
+    subset, smoothed = datasets(obj, data, default, extend, sigma)
+    
+    maxval = np.mean(smoothed) + np.std(smoothed)
+    mid = smoothed[smoothed.size // 2]
+    return mid > maxval
+
+    #for i in range(subset.size):
+    #    if subset[i] > smoothed[i]: pass
+    #        #subset[i] = smoothed[i]
+    #return (subset, smoothed)
+
+def datasets(obj, data, default, extend, sigma):
+    xmin = int(obj['xmin']) - extend
+    xmin = xmin if xmin > default[0] else default[0]
+    xmax = int(obj['xmax']) + extend
+    xmax = xmax if xmax < default[1] else default[1]
+    
+    subset = data[int(obj['y']), xmin:xmax]
+    #ash = np.arcsinh(subset)
+    smoothed = signal.cspline1d(subset, sigma)
+    
+    return (subset, smoothed)
 
 
 
@@ -74,6 +103,6 @@ def main():
 
     data = get_img_data(args.coordinates, butler)
     objects, *_ = get_objs(data)
-    print(find_lbg(objects))
+    print(find_lbg(objects, data))
 
 if __name__ == "__main__": main()
