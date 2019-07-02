034c97b8066854bf1591b747005667a2ab64bbe4
telastrus <10052313+telastrus@users.noreply.github.com>
2019-07-02 17:05:38 -0400

    made bad reader framework

--- ashd/pipelinev2/reader.py
+++ ashd/pipelinev2/reader.py
@@ -1,4 +1,6 @@
 import sqlite3
+import numpy as np
+from astropy.io import fits
 
 SEP_DTYPE = [('thresh', '<f8'), ('npix', '<i8'), ('tnpix', '<i8'), ('xmin', '<i8'), ('xmax', '<i8'), ('ymin', '<i8'),
     ('ymax', '<i8'), ('x', '<f8'), ('y', '<f8'), ('x2', '<f8'), ('y2', '<f8'), ('xy', '<f8'), ('errx2', '<f8'),
@@ -6,13 +8,26 @@ SEP_DTYPE = [('thresh', '<f8'), ('npix', '<i8'), ('tnpix', '<i8'), ('xmin', '<i8
     ('cxy', '<f8'), ('cflux', '<f8'), ('flux', '<f8'), ('cpeak', '<f8'), ('peak', '<f8'), ('xcpeak', '<i8'), ('ycpeak', '<i8'),
     ('xpeak', '<i8'), ('ypeak', '<i8'), ('flag', '<i8')]
 
+SQL_DTYPE=[("id", "<i8"), ("ra", "<f8"), ("dec", "<f8")]
+
 class Reader:
     def __init__(self, target, autoload = True):
         self.target = target
         self.conn = sqlite3.connect(f"sqlite:///{target}/db")
-        self.c = self.conn.cursor()
+        if autoload: self.load()
     
-    @property
-    def size(self): pass
+    def load(self):
+        c = self.conn.cursor()
+        self.data = []
+        for row in c.execute("select * from findings"):
+            d = np.fromstring(row[4], dtype=SEP_DTYPE)
+            row[4] = d
+            self.data.append(row)
+    
+    def get_img(self, id):
+        fname = id
+        if type(id) == int:
+            fname = np.base_repr(id, 36)
+        return fits.open(fname)
 
-    def iter(self): pass
+        
