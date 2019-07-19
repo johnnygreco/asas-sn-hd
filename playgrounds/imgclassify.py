38323934f4371d18d2cce38f3f75aab684edd2b8
telastrus <10052313+telastrus@users.noreply.github.com>
2019-07-19 16:56:30 -0400

    made saving a proper thing

--- playgrounds/imgclassify.py
+++ playgrounds/imgclassify.py
@@ -31,11 +31,23 @@ def add(state):
 y = lambda: add(True)
 n = lambda: add(False)
 
-def done():
-    global data
+def load():
+    global data, count
+    f = open("state.bin", 'rb')
+    data = pickle.load(f)
+    f.close()
+    count = len(data)
+
+def save():
+    global data, app
     f = open("state.bin", 'wb')
     pickle.dump(data, f)
     f.close()
+    app.setLabel("iter", f"{count}/{total}; saved")
+
+def done():
+    global data
+    save()
     print("Done, closing")
     sys.exit(0)
 
@@ -44,16 +56,19 @@ def back():
     count -= 1
     update()
 
+load()
 app = gui("plz end me ty")
 app.addImage("Img", fn())
 app.addLabel("iter", f"{count}/{total}")
 app.addButton("Yes", y)
 app.addButton("No", n)
 app.addButton("Back", back)
+app.addButton("Save", save)
 app.addButton("Done", done)
 app.bindKey("y", y)
 app.bindKey("n", n)
 app.bindKey("d", done)
+app.bindKey("s", save)
 app.bindKey("b", back)
 
 app.go()
